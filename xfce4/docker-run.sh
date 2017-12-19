263179722e96574ed28262f00d591c5281add403
root <root@localhost.localdomain>
2017-12-19 02:49:45 -0200

    just adjusting xfce4 image name

--- xfce4/docker-run.sh
+++ xfce4/docker-run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-CONTAINER_NAME=fzl_cinelerra;
+CONTAINER_NAME=fzl_xfce4;
 
 docker rm $CONTAINER_NAME
 
@@ -8,7 +8,7 @@ docker run  \
        --name $CONTAINER_NAME -d  \
        -p 5901:5901 \
        -h $CONTAINER_NAME  \
-       --net fzl_network_bridge --ip 192.168.33.158   wagnermarques/fzl_cinelerra:0.0.1
+       --net fzl_network_bridge --ip 192.168.33.158   wagnermarques/fzl_xfce4:0.0.1
 
 
        docker logs $CONTAINER_NAME

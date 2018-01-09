aca18e4784810773df2a6919b3c8ec32a56904f9
wagnermarques <wagnerdocri@gmail.com>
2018-01-09 17:21:38 -0200

    adjust tomcat8 url for download

--- mariadb-not-working/scripts/start.sh
+++ mariadb-not-working/scripts/start.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-if [ -x /scripts/config_mariadb.sh ]; then
-  # Initial configuration
-  /scripts/config_mariadb.sh || exit 1
-fi
+#if [ -x /scripts/config_mariadb.sh ]; then
+#  # Initial configuration
+#  /scripts/config_mariadb.sh || exit 1
+#fi
 
-rm -f /run/mysqld/mysqld.sock
+#rm -f /run/mysqld/mysqld.sock
 exec /usr/bin/mysqld_safe

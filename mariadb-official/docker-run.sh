ec8a157fd61b267147605be7b2a73a8b62de9cc7
wagnermarques <wagnerdocri@gmail.com>
2018-01-26 12:29:17 -0200

    starting bash scripts to configure liferay with mysql container

--- mariadb-official/docker-run.sh
+++ mariadb-official/docker-run.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 #https://hub.docker.com/_/mariadb/
 
-CONTAINER_NAME=mariadb
+CONTAINER_NAME=$1
+HOST_PORT=$2
+CONTAINER_IP=$3
 
+if [ "x$CONTAINER_NAME" = "x" ]; then
+    {
+        $CONTAINER_NAME=mariadb
+        $HOST_PORT=3306
+        $CONTAINER_IP=192.168.33.151
+    }
+fi
+
+echo "Running... docker-run.sh <$CONTAINER_NAME> at host port <$HOST_PORT> and ip <$CONTAINER_IP>."
 mkdir -p $(pwd)/container_shared_folder/$CONTAINER_NAME #data dir is for each respective container name
 
 docker run \
-       --name mariadb \
-       --net fzl_network_bridge --ip 192.168.33.151 \
+       --name  $CONTAINER_NAME -h $CONTAINER_NAME  \
+       --net fzl_network_bridge --ip $CONTAINER_IP  \
        -v $(pwd)/container_shared_folder/$CONTAINER_NAME:/var/lib/mysql \
        -v $(pwd)/sqlscripts:/sqlscripts \
-       -h mariadb -p 3306:3306 \
+       -p $HOST_PORT:3306 \
        -e MYSQL_ROOT_PASSWORD=admin123 \
        -d mariadb:10.3 
 

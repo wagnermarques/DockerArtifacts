b21979c109123fd79aad31c84eb30d33931be577
wagnerdocri@gmail.com <wagnerdocri@gmail.com>
2018-07-26 12:03:47 -0300

    o orbeon forms funciona o jee security juntamente com o orbeonwebfragment que resolve limitacoes da url-patter solicitando login do usuario para o summary de qualquer form. O new de qualquer form esta permitido sem login. O realm de usuarios do tomcat esta no posgresql

--- orbeonforms/docker-run.sh
+++ orbeonforms/docker-run.sh
@@ -7,6 +7,10 @@ CONTAINER_IP=$3
 mkdir -p $(pwd)/../orbeonforms-containers-shared-dir/$CONTAINER_NAME #dir for each liferay containers shared folders
 SHARED_FOLDER=$(pwd)/../orbeonforms-containers-shared-dir/$CONTAINER_NAME
 
+mkdir -p $(pwd)/../orbeonforms-exist-data
+EXIST_DATA=$(pwd)/../orbeonforms-exist-data
+
+
 if [ "x$CONTAINER_NAME" = "x" ]; then
     {
         CONTAINER_NAME=fzl_orbeonforms
@@ -14,6 +18,7 @@ if [ "x$CONTAINER_NAME" = "x" ]; then
         CONTAINER_IP=192.168.33.170
     }
 fi
+
 
 echo "Running... orbeonforms/docker-run.sh <$CONTAINER_NAME> at host port <$HOST_PORT> and ip <$CONTAINER_IP>."
 IMG_NAME=wagnermarques/fzl_orbeonforms:0.0.1
@@ -23,6 +28,7 @@ docker run \
        --name "$CONTAINER_NAME" -h "$CONTAINER_HOST_NAME" \
        --net fzl_network_bridge --ip "$CONTAINER_IP" \
        -v "$SHARED_FOLDER":/orbeonforms-containers-shared-dir/$CONTAINER_NAME \
+       -v "$EXIST_DATA":/exist_data \
        -p "$HOST_PORT":8080 \
        -d "$IMG_NAME"
        

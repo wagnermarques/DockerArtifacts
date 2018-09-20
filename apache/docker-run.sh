8a6951255e0c85c6a153e64abeb0756d3f68e5a8
wagnermarques <wagnerdocri@gmail.com>
2018-09-20 06:10:47 -0400

    ajuste na porta do apache pro docker abrir a porta corretamente no host

--- apache/docker-run.sh
+++ apache/docker-run.sh
@@ -1,35 +1,24 @@
-!/bin/bash
+#!/bin/bash
 
 CONTAINER_NAME=fzl_apache;
-VOLUME_PATH1=$1;
+CONTAINER_IP=192.168.33.100
+CONTAINER_SHARED_FOLDER="$(pwd)/../apache-containers-shared-dir/www/html"   
 
-# Change it accordingle of your needs
-#CONTAINER_DEFAULT_WWW_HTML="/run/media/wagner/96fea5f1-d297-4f63-a035-abf6511467be/wagnerdocri@gmail.com2/envs/env-dev/sources/dojo-fzl-demos/";
-CONTAINER_DEFAULT_WWW_HTML="/run/media/wagner/51d54d26-34c8-4671-8da1-c12adc7a5a2c/wagnerdocri@gmail.com2/envs/env-dev-git-clones/dojotoolkitSourcesAndDemos/dojotoolkit"
-
-if [ "X$VOLUME_PATH" = "X" ]; then
-    echo "--- Hey, you do not provide a /var/www/html volume...";
-    echo "--- Using...";
-    echo "--- $CONTAINER_DEFAULT_WWW_HTML"
-    echo "--- and /run/media/wagner/96fea5f1-d297-4f63-a035-abf6511467be/wagnerdocri@gmail.com2/envs/env-dev/sources/dojo-fzl-demos/"
-    echo "--- because a symbolic links I use";
-    echo "--- !change it to your web app dir!";
-    #if you do not provide a /var/www/html volume
-    #this will be provide as default
-    #change it to your default dir, where there are your web app
-    VOLUME_PATH1="$CONTAINER_DEFAULT_WWW_HTML"
-fi;
-
-
-docker rm $CONTAINER_NAME
-
+mkdir -p "$CONTAINER_SHARED_FOLDER"
 
 docker run \
        --name $CONTAINER_NAME -d \
-       -h fzl_apache \
-       -p 80:8080 \
-       -v "$VOLUME_PATH1":/var/www/html \
-       -v "$VOLUME_PATH2":/run/media/wagner/96fea5f1-d297-4f63-a035-abf6511467be/wagnerdocri@gmail.com2/envs/env-dev/sources/dojo-fzl-demos/ \
+       -h $CONTAINER_NAME \
+       --net fzl_network_bridge --ip "$CONTAINER_IP" \
+       -p 18080:80 \
+       -v $(pwd)/../apache-containers-shared-dir/www/html:/var/www/html \
        wagnermarques/fzl_apache 
        
-docker logs $CONTAINER_NAME
+
+echo Container name: $CONTAINER_NAME
+echo Container ip: $CONTAINER_IP
+echo Container host ports:  $HOST_PORT
+echo SHARED_FOLDER: $SHARED_FOLDER
+echo http://localhost:$HOST_PORT
+
+

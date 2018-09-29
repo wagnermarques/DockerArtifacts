446986e58207746be6b3653d96239b90cd2e6b07
wagnermarques <wagnerdocri@gmail.com>
2018-09-29 09:18:21 -0400

    try to make phpinfo works, but no success at this moment

--- apache/docker-run.sh
+++ apache/docker-run.sh
@@ -1,24 +1,29 @@
 #!/bin/bash
 
 CONTAINER_NAME=fzl_apache;
-CONTAINER_IP=192.168.33.100
+CONTAINER_IP=192.168.33.105
 CONTAINER_SHARED_FOLDER="$(pwd)/../apache-containers-shared-dir/www/html"   
 
-mkdir -p "$CONTAINER_SHARED_FOLDER"
+echo "$CONTAINER_SHARED_FOLDER" 
+if [ ! -d "$CONTAINER_SHARED_FOLDER" ]; then
+    mkdir -p "$CONTAINER_SHARED_FOLDER"
+fi 
+
+
 
 docker run \
        --name $CONTAINER_NAME -d \
-       -h $CONTAINER_NAME \
+	-h localhost \
        --net fzl_network_bridge --ip "$CONTAINER_IP" \
-       -p 18080:80 \
+       -p 9900:80 \
        -v $(pwd)/../apache-containers-shared-dir/www/html:/var/www/html \
-       wagnermarques/fzl_apache 
+       -v $(pwd)/../apache-containers-shared-dir/var/log/apache2:/var/log/apache2 \
+	wagnermarques/fzl_apache 
        
 
 echo Container name: $CONTAINER_NAME
 echo Container ip: $CONTAINER_IP
-echo Container host ports:  $HOST_PORT
-echo SHARED_FOLDER: $SHARED_FOLDER
+echo Container host ports:  9900:80
+echo SHARED_FOLDER: $CONTAINER_SHARED_FOLDER
 echo http://localhost:$HOST_PORT
-
 

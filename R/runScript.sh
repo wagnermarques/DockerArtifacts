9f8d038a23f949b4cfa1328ce564be5e644fea32
wagnermarquer <wagnerdocri@gmail.com>
2021-02-19 14:30:34 -0300

    added example_script.R e runScript.sh pra dar suporte a rodar scripts R no container R

---
+++ R/runScript.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#
+# Usage:
+# runScript.sh /path/to/R/script.R
+# 
+# This script will run script.R insite container
+# The container name will be the name of the R script file
+# The container will be removed after script runs
+#
+SCRIPT=$1
+invertedScriptFilePath=$(rev <<< $1);
+OLDIFS="$IFS"
+IFS='/' #  (/) is set as delimiter
+
+function extractScriptFileNameAndScriptDirectoryPath(){
+
+    #inverted script file path is readed in a array with IFS='/' file delimiter
+    read -ra arrScriptPathInv <<< $invertedScriptFilePath
+    IFS=$OLDIFS #after read command we can set IFS alone
+    
+    scriptFileName=$(rev <<< ${arrScriptPathInv[0]})
+
+    i=${#arrScriptPathInv}
+    until [[ $i -eq 0 ]]
+    do
+        scriptPathDir="$scriptPathDir/$(rev <<< ${arrScriptPathInv[$i]})"
+        ((i--))
+    done    
+}
+
+extractScriptFileNameAndScriptDirectoryPath
+
+CONTAINER_NAME=$scriptFileName
+SOURCE_DIR=$scriptPathDir
+IMG_NAME="wagnermarques/fzl_r:0.0.1"
+
+docker run -it --rm --name $CONTAINER_NAME -v $SOURCE_DIR:/sources --workdir /sources $IMG_NAME Rscript --default-packages=methods,utils,grDevices,stats -e 'sessionInfo()' $scriptFileName
